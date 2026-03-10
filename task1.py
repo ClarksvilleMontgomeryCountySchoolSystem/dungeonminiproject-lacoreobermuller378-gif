b26344a3094418c86cb1cef78f90ef632a9706c6
lacoreobermuller378-gif <lacore.obermuller378@students.cmcss.net>
2026-03-10 14:20:29 -0500

    finished

--- task1.py
+++ task1.py
@@ -1,8 +1,7 @@
-from task2 import outcome
 
-torch_lit = True       torch_lit: True
+torch_lit = True
  if torch_lit:
      outcome = "flicker: the flame will take me to the corridor"
  else:
      outcome = "Doom: Darkness consume the room the path"
-     print(outcome)
+print(outcome)
